e2d2f839800d048f7549595bd48e5f5ec83559f9
Lyndsea Warkenthien <lwarkenthien24@gmail.com>
2023-05-10 21:49:20 -0700

    added matches screen
    can get to the matches screen from the profile screen, and then back to the profile screen

--- cs278project/screens/ProfileScreen.js
+++ cs278project/screens/ProfileScreen.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }) => {
+  const handlePress = () => {
+    navigation.navigate("Matches");
+  };
+
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Screen</Text>
+      <Button title="Go to Matches" onPress={handlePress} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
 
 export default ProfileScreen;
