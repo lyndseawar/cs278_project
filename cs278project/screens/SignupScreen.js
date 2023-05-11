57505d58f559f617a3828939c22e04839da79170
nickhafer <107949449+nickhafer@users.noreply.github.com>
2023-05-11 14:28:43 -0700

    added signup screen

--- cs278project/screens/SignupScreen.js
+++ cs278project/screens/SignupScreen.js
@@ -1,12 +1,157 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from "react";
+// import { View, Text } from "react-native";
 
-const SignupScreen = () => {
+// const SignupScreen = () => {
+//   return (
+//     <View>
+//       <Text>Signup Screen</Text>
+//     </View>
+//   );
+// };
+
+// export default SignupScreen;
+
+
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Alert,
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = () => {
+    if (password !== confirmPassword) {
+      Alert.alert("Error", "Passwords do not match");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      Alert.alert("Error", "Invalid email address");
+      return;
+    }
+
+    // Add your logic for creating a new account here
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // If account creation is successful, navigate to the profile screen
+    navigation.navigate("Profile");
+  };
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   return (
-    <View>
-      <Text>Signup Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Platedate</Text>
+        <Text style={styles.tagline}>
+          the best way to set your next platonic date
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Email</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholder="Enter your email"
+          placeholderTextColor="#aaa"
+        />
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+          placeholder="Enter your password"
+          placeholderTextColor="#aaa"
+        />
+        <Text style={styles.inputLabel}>Confirm Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+          secureTextEntry
+          placeholder="Confirm your password"
+          placeholderTextColor="#aaa"
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={handleSignup}>
+          <Text style={styles.loginButtonText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+  },
+  titleContainer: {
+    height: height * 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 48,
+    fontFamily: "Poppins-Bold",
+  },
+  tagline: {
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+  },
+  inputContainer: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: width * 0.1,
+  },
+  inputLabel: {
+    fontSize: 16,
+    fontFamily: "Poppins-Regular",
+    marginBottom: 8,
+  },
+  input: {
+    borderBottomWidth: 1.5,
+    borderBottomColor: "#4B0082",
+    marginBottom: 30,
+    paddingLeft: 2,
+    fontSize: 16,
+    width: width * 0.8,
+  },
+  loginButton: {
+    width: width * 0.4,
+    backgroundColor: "#4B0082",
+    borderRadius: 50,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    alignSelf: "center",
+    marginTop: 50,
+  },
+  loginButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontSize: 18,
+    fontFamily: "Poppins-Bold",
+  },
+});
 
 export default SignupScreen;
