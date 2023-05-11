9ec104a0a5034a63490f419420c2d1fbbd91ddca
Lyndsea Warkenthien <lwarkenthien24@gmail.com>
2023-05-10 20:00:29 -0700

    made the welcome screen

--- cs278project/navigation/AppNavigator.js
+++ cs278project/navigation/AppNavigator.js
@@ -7,24 +7,15 @@ import SignupScreen from "../screens/SignupScreen";
 import HomeScreen from "../screens/HomeScreen";
 import MatchesScreen from "../screens/MatchesScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
-const AuthStack = createStackNavigator();
 const AppStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
-function AuthStackNavigator() {
-  return (
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="Login" component={LoginScreen} />
-      <AuthStack.Screen name="Signup" component={SignupScreen} />
-    </AuthStack.Navigator>
-  );
-}
 
 function AppTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Matches" component={MatchesScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
@@ -34,10 +25,12 @@ function AppTabs() {
 function AppStackNavigator() {
   return (
     <AppStack.Navigator
-      initialRouteName="Auth"
-      screenOptions={{ headerShown: false }}
+      initialRouteName="Welcome"
+      screenOptions={{ headerMode: 'screen', headerShown: false }}
     >
-      <AppStack.Screen name="Auth" component={AuthStackNavigator} />
+      <AppStack.Screen name="Welcome" component={WelcomeScreen} />
+      <AppStack.Screen name="Login" component={LoginScreen} />
+      <AppStack.Screen name="Signup" component={SignupScreen} />
       <AppStack.Screen name="App" component={AppTabs} />
     </AppStack.Navigator>
   );
---
+++ cs278project/screens/WelcomeScreen.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { AppLoading } from "expo";
+
+const { width, height } = Dimensions.get("window");
+
+const WelcomeScreen = ({ navigation }) => {
+  const handleSignUp = () => {
+    navigation.navigate("Signup");
+  };
+
+  const handleSignIn = () => {
+    navigation.navigate("Login");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Plates</Text>
+        <Text style={styles.tagline}>
+          The best way to set your next platonic date
+        </Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
+          <Text style={styles.signUpButtonText}>Create Account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSignIn}>
+          <Text style={styles.signInText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 48,
+    fontFamily: "Poppins-Bold",
+  },
+  tagline: {
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+  },
+  bottomContainer: {
+    paddingBottom: height / 8,
+  },
+  signUpButton: {
+    width: width * 0.8,
+    backgroundColor: "#4B0082",
+    borderRadius: 50,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    marginBottom: 12,
+  },
+  signUpButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+  },
+  signInText: {
+    fontSize: 16,
+    color: "#4B0082",
+    fontFamily: "Poppins-Regular",
+  },
+});
+
+export default WelcomeScreen;
