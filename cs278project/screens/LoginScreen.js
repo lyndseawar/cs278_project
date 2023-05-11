f0d3e8014e1f08218882c1dac458318a13defd41
Lyndsea Warkenthien <lwarkenthien24@gmail.com>
2023-05-10 21:15:02 -0700

    login screen made
    uses no logic and just logs in, takes you to the profile screen when you press the login button

--- cs278project/screens/LoginScreen.js
+++ cs278project/screens/LoginScreen.js
@@ -1,12 +1,117 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
-const LoginScreen = () => {
+const { width, height } = Dimensions.get("window");
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Add your logic for authenticating the user here
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // If authentication is successful, navigate to the profile screen
+    navigation.navigate("Profile");
+  };
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Platedate</Text>
+        <Text style={styles.tagline}>
+          the best way to set your next platonic date
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Email</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholder="Enter your email"
+          placeholderTextColor="#aaa"
+        />
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+          placeholder="Enter your password"
+          placeholderTextColor="#aaa"
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+  },
+  titleContainer: {
+    height: height * 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 48,
+    fontFamily: "Poppins-Bold",
+  },
+  tagline: {
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+  },
+  inputContainer: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: width * 0.1,
+  },
+  inputLabel: {
+    fontSize: 16,
+    fontFamily: "Poppins-Regular",
+    marginBottom: 8,
+  },
+  input: {
+    borderBottomWidth: 1.5,
+    borderBottomColor: "#4B0082",
+    marginBottom: 30,
+    paddingLeft: 2,
+    fontSize: 16,
+    width: width * 0.8,
+  },
+  loginButton: {
+    width: width * 0.4,
+    backgroundColor: "#4B0082",
+    borderRadius: 50,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    alignSelf: "center",
+    marginTop: 50,
+  },
+  loginButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+  },
+});
 
 export default LoginScreen;
