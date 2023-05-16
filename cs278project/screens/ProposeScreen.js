c46efef3f2c5b6c750817ae45d16038ca85b8a95
nickhafer <107949449+nickhafer@users.noreply.github.com>
2023-05-15 22:59:46 -0700

    scrollview spacing in propose screen

--- cs278project/screens/ProposeScreen.js
+++ cs278project/screens/ProposeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, SafeAreaView, TouchableOpacity, Dimensions} from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, SafeAreaView, TouchableOpacity, Dimensions, ScrollView} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 // import DatePicker from 'react-native-date-picker'
@@ -42,7 +42,8 @@ const ProposeScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.section}>
+      <ScrollView>
+        <View style={styles.section}>
         <Text style={styles.sectionTitle}>Activity</Text>
         <TextInput
           style={styles.input}
@@ -50,66 +51,66 @@ const ProposeScreen = () => {
           onChangeText={setActivity}
           placeholder="Add Activity"
         />
-      </View>
-      <View style={styles.section}>
-        <View style={styles.sectionTitle}>
-          <Ionicons name="calendar" size={32} />
-          <Text style={styles.sectionTitle}> Date</Text>
         </View>
-        <TextInput
-          style={styles.input}
-          placeholder="Monday, May 15th"
-          value={date}
-          onChangeText={setDate}
-        />
-      </View>
-      <View style={styles.section}>
-        <View style={styles.sectionTitle}>
-          <Ionicons name="time" size={32} />
-          <Text style={styles.sectionTitle}> Time</Text>
+        <View style={styles.section}>
+          <View style={styles.sectionTitle}>
+            <Ionicons name="calendar" size={32} />
+            <Text style={styles.sectionTitle}> Date</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Monday, May 15th"
+            value={date}
+            onChangeText={setDate}
+          />
         </View>
-        <TextInput
-          style={styles.input}
-          placeholder="12:30pm - 1:30pm"
-          value={time}
-          onChangeText={setTime}
-        />
-      </View>
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Guests Needed</Text>
-        <TouchableOpacity onPress={showPicker} style={styles.pickerContainer}>
-          <Text style={styles.pickerText}> {guests}</Text>
-        </TouchableOpacity>
-        {isPickerVisible && (
-          <Picker
-            style={styles.picker}
-            selectedValue={guests}
-            onValueChange={handlePickerValueChange}
-          >
-            <Picker.Item label="1" value="1" />
-            <Picker.Item label="2" value="2" />
-            <Picker.Item label="3" value="3" />
-            <Picker.Item label="4" value="4" />
-            <Picker.Item label="5" value="5" />
-            <Picker.Item label="6" value="6" />
-            <Picker.Item label="7" value="7" />
-            <Picker.Item label="8" value="8" />
-            <Picker.Item label="9" value="9" />
-            <Picker.Item label="10" value="10" />
-            {/* Add more options as needed */}
-          </Picker>
-        )}
-      </View>
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Location</Text>
-        <TextInput
-          style={styles.input}
-          value={location}
-          onChangeText={setLocation}
-          placeholder="Add Location"
-        />
-      </View>
-      <View style={styles.bottomContainer}>
+        <View style={styles.section}>
+          <View style={styles.sectionTitle}>
+            <Ionicons name="time" size={32} />
+            <Text style={styles.sectionTitle}> Time</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="12:30pm - 1:30pm"
+            value={time}
+            onChangeText={setTime}
+          />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Guests Needed</Text>
+          <TouchableOpacity onPress={showPicker} style={styles.pickerContainer}>
+            <Text style={styles.pickerText}> {guests}</Text>
+          </TouchableOpacity>
+          {isPickerVisible && (
+            <Picker
+              style={styles.picker}
+              selectedValue={guests}
+              onValueChange={handlePickerValueChange}
+            >
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
+              <Picker.Item label="6" value="6" />
+              <Picker.Item label="7" value="7" />
+              <Picker.Item label="8" value="8" />
+              <Picker.Item label="9" value="9" />
+              <Picker.Item label="10" value="10" />
+              {/* Add more options as needed */}
+            </Picker>
+          )}
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Location</Text>
+          <TextInput
+            style={styles.input}
+            value={location}
+            onChangeText={setLocation}
+            placeholder="Add Location"
+          />
+        </View>
+        <View style={styles.bottomContainer}>
           <TouchableOpacity
             style={styles.DoneButton}
             //onPress={() => navigation.navigate("FeedScreen")}
@@ -117,6 +118,12 @@ const ProposeScreen = () => {
             <Text style={styles.DoneButtonText}>Done</Text>
           </TouchableOpacity>
         </View>
+        <View>
+          {/* These are just here to add spacing for the scrollview */}
+          <Text></Text>
+          <Text></Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
