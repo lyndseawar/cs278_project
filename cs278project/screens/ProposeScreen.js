0361d6975bbe2688f7f020712e3a8f4595aa440a
tiffanyleee <76767856+tiffanyleee@users.noreply.github.com>
2023-05-15 19:26:11 -0700

    textbox

--- cs278project/screens/ProposeScreen.js
+++ cs278project/screens/ProposeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity, TextInput, SafeAreaView} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -7,13 +7,24 @@ const ProposeScreen = ({ navigation }) => {
     const handlePress = () => {
       navigation.navigate("Matches");
     };
+    const [number, onChangeNumber] = React.useState('');
     
   
     return (
+    
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
       <View style={styles.container}>
-        <Text style={styles.title}>Propose Screen</Text>
+        <Text style={styles.title}>Activity:</Text>
         <Button title="Go to Matches" onPress={handlePress} />
       </View>
+    </SafeAreaView>
     );
   };
   
@@ -28,6 +39,12 @@ const ProposeScreen = ({ navigation }) => {
       fontWeight: "bold",
       marginBottom: 20,
     },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },  
   });
   
   export default ProposeScreen;
