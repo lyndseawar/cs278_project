3e57312ad3f56d6e1793fe40d37601cdd5432a88
nickhafer <107949449+nickhafer@users.noreply.github.com>
2023-06-05 21:58:31 -0700

    new signup data is added to the backend

--- cs278project/screens/SignupScreen.js
+++ cs278project/screens/SignupScreen.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Dimensions,
   Alert,
+  ScrollView,
 } from "react-native";
 import { Formik } from "formik";
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -24,12 +25,15 @@ const createUserDocument = async (user, additionalData) => {
   const snapshot = await getDoc(userRef);
   //if there isn't any data there, create it
   if (!snapshot.exists()) {
-    const { displayName, joinDate } = additionalData;
+    const { displayName, joinDate, thisYear, showerThought, nerdiestThing } = additionalData;
     try {
       await setDoc(userRef, {
         displayName,
         email: user.email,
         joinDate,
+        thisYear,
+        showerThought,
+        nerdiestThing,
         //any other data that we want to store
       });
       return getUserDocument(user.uid);
@@ -59,7 +63,7 @@ const { width, height } = Dimensions.get("window");
 export const SignupScreen = ({ navigation }) => {
   const [errorState, setErrorState] = useState("");
   const handleSignup = async (values) => {
-    const { email, password, firstName, lastName } = values;
+    const { email, password, firstName, lastName, thisYear, showerThought, nerdiestThing } = values;
     const displayName = `${firstName} ${lastName}`;
     try {
       const userCredential = await createUserWithEmailAndPassword(
@@ -71,6 +75,9 @@ export const SignupScreen = ({ navigation }) => {
       await createUserDocument(user, {
         displayName,
         joinDate: new Date().toDateString(),
+        thisYear,
+        showerThought,
+        nerdiestThing,
       });
       if (!userDoc) {
         Alert.alert("Error", "Something went wrong signing up");
@@ -94,12 +101,20 @@ export const SignupScreen = ({ navigation }) => {
       <View style={styles.inputContainer}>
         <Formik
           initialValues={{
+            // login data
             firstName: "",
             lastName: "",
-            joinDate: new Date(), //current data
             email: "",
             password: "",
             confirmPassword: "",
+
+            //profile data
+            thisYear: "",
+            showerThought: "",
+            nerdiestThing: "",
+
+            // extra data
+            joinDate: new Date(), //current data
           }}
           validationSchema={signupValidationSchema}
           onSubmit={(values) => handleSignup(values)}
@@ -113,85 +128,133 @@ export const SignupScreen = ({ navigation }) => {
             handleBlur,
           }) => (
             <>
-              {/* Input fields */}
-              <TextInput
-                style={styles.input}
-                name="firstName"
-                placeholder="First Name"
-                autoCapitalize="none"
-                autoFocus={true}
-                onChangeText={handleChange("firstName")}
-                onBlur={handleBlur("firstName")}
-                value={values.firstName}
-              />
-              <FormErrorMessage
-                error={errors.firstName}
-                visible={touched.firstName}
-              />
-              <TextInput
-                style={styles.input}
-                name="lastName"
-                placeholder="Last Name"
-                autoCapitalize="none"
-                autoFocus={true}
-                onChangeText={handleChange("lastName")}
-                onBlur={handleBlur("lastName")}
-                value={values.lastName}
-              />
-              <FormErrorMessage
-                error={errors.lastName}
-                visible={touched.lastName}
-              />
-              <TextInput
-                style={styles.input}
-                name="email"
-                leftIconName="email"
-                placeholder="Enter email"
-                autoCapitalize="none"
-                keyboardType="email-address"
-                textContentType="emailAddress"
-                autoFocus={true}
-                value={values.email}
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-              />
-              <FormErrorMessage error={errors.email} visible={touched.email} />
-              <TextInput
-                style={styles.input}
-                name="password"
-                leftIconName="key-variant"
-                placeholder="Enter password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                textContentType="newPassword"
-                value={values.password}
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-              />
-              <FormErrorMessage
-                error={errors.password}
-                visible={touched.password}
-              />
-              <TextInput
-                style={styles.input}
-                name="confirmPassword"
-                leftIconName="key-variant"
-                placeholder="Confirm password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                textContentType="password"
-                value={values.confirmPassword}
-                onChangeText={handleChange("confirmPassword")}
-                onBlur={handleBlur("confirmPassword")}
-              />
-              <FormErrorMessage
-                error={errors.confirmPassword}
-                visible={touched.confirmPassword}
-              />
-              {/* Display Screen Error Mesages */}
-              {errorState !== "" ? (
-                <FormErrorMessage error={errorState} visible={true} />
-              ) : null}
+              <ScrollView>
+                {/* Input fields */}
+                <TextInput
+                  style={styles.input}
+                  name="firstName"
+                  placeholder="First Name"
+                  autoCapitalize="none"
+                  autoFocus={true}
+                  onChangeText={handleChange("firstName")}
+                  onBlur={handleBlur("firstName")}
+                  value={values.firstName}
+                />
+                <FormErrorMessage
+                  error={errors.firstName}
+                  visible={touched.firstName}
+                />
+                <TextInput
+                  style={styles.input}
+                  name="lastName"
+                  placeholder="Last Name"
+                  autoCapitalize="none"
+                  autoFocus={true}
+                  onChangeText={handleChange("lastName")}
+                  onBlur={handleBlur("lastName")}
+                  value={values.lastName}
+                />
+                <FormErrorMessage
+                  error={errors.lastName}
+                  visible={touched.lastName}
+                />
+                <TextInput
+                  style={styles.input}
+                  name="email"
+                  leftIconName="email"
+                  placeholder="Enter email"
+                  autoCapitalize="none"
+                  keyboardType="email-address"
+                  textContentType="emailAddress"
+                  autoFocus={true}
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                />
+                <FormErrorMessage error={errors.email} visible={touched.email} />
+                <TextInput
+                  style={styles.input}
+                  name="password"
+                  leftIconName="key-variant"
+                  placeholder="Enter password"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  textContentType="newPassword"
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                />
+                <FormErrorMessage
+                  error={errors.password}
+                  visible={touched.password}
+                />
+                <TextInput
+                  style={styles.input}
+                  name="confirmPassword"
+                  leftIconName="key-variant"
+                  placeholder="Confirm password"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  textContentType="password"
+                  value={values.confirmPassword}
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={handleBlur("confirmPassword")}
+                />
+                <FormErrorMessage
+                  error={errors.confirmPassword}
+                  visible={touched.confirmPassword}
+                />
+
+                {/* Profile Data */}
+                <TextInput
+                  style={styles.input}
+                  name="thisYear"
+                  placeholder="This year I want to..."
+                  autoCapitalize="none"
+                  autoFocus={true}
+                  onChangeText={handleChange("thisYear")}
+                  onBlur={handleBlur("thisYear")}
+                  value={values.thisYear}
+                />
+                <FormErrorMessage
+                  error={errors.thisYear}
+                  visible={touched.thisYear}
+                />
+                <TextInput
+                  style={styles.input}
+                  name="showerThought"
+                  placeholder="A shower thought I've had..."
+                  autoCapitalize="none"
+                  autoFocus={true}
+                  onChangeText={handleChange("showerThought")}
+                  onBlur={handleBlur("showerThought")}
+                  value={values.showerThought}
+                />
+                <FormErrorMessage
+                  error={errors.showerThought}
+                  visible={touched.showerThought}
+                />
+                <TextInput
+                  style={styles.input}
+                  name="nerdiestThing"
+                  placeholder="The nerdiest thing about me..."
+                  autoCapitalize="none"
+                  autoFocus={true}
+                  onChangeText={handleChange("nerdiestThing")}
+                  onBlur={handleBlur("nerdiestThing")}
+                  value={values.nerdiestThing}
+                />
+                <FormErrorMessage
+                  error={errors.nerdiestThing}
+                  visible={touched.nerdiestThing}
+                />
+                
+                {/* Display Screen Error Mesages */}
+                {errorState !== "" ? (
+                  <FormErrorMessage error={errorState} visible={true} />
+                ) : null}
+              </ScrollView>
+
               {/* Signup button */}
               <TouchableOpacity
                 style={styles.loginButton}
@@ -202,6 +265,7 @@ export const SignupScreen = ({ navigation }) => {
             </>
           )}
         </Formik>
+
         {/* bottom container */}
         <View style={styles.bottomContainer}>
           <TouchableOpacity
